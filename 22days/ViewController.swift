863b80632c0f58377cbbaaf8bf8afd829122fef0
Mary Fan <maryfan1106@gmail.com>
2020-05-08 22:52:02 -0400

    Initial Commit

---
+++ 22days/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  22days
+//
+//  Created by Mary Fan on 5/8/20.
+//  Copyright © 2020 Mary Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
