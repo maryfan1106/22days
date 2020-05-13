dfeeb03fde889b9dd467e647c9e9effbe8bc8461
Mary Fan <maryfan1106@gmail.com>
2020-05-13 04:45:23 -0400

    updated ui for newStoryView

--- 22days/Screens/NewStoryViewController.swift
+++ 22days/Screens/NewStoryViewController.swift
@@ -15,20 +15,17 @@ protocol AddNewStoryDelegate {
 }
 
 class NewStoryViewController: UIViewController {
-    
     var addNewStoryDelegate: AddNewStoryDelegate!
-
+    var newStory = Story(date: "May 15, 2020", title: "Moody Purples?", mood: "happy", reason: "school", story: "Last day of school!! Finally graduating 😭😭", backgroundImage: "image10")
+    
     @IBOutlet weak var postButton: UIButton!
     @IBAction func postButtonTapped(_ sender: UIButton) {
-        addNewStoryDelegate.didTapPost(story:
-            Story(date: "May 12, 2020", title: "Moody Purples?", mood: "sad", reason: "school", story: "iOS dev is killing me.", backgroundImage: "image10")
-        )
+        addNewStoryDelegate.didTapPost(story: newStory)
         AudioServicesPlayAlertSound(1306)
         dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 }
